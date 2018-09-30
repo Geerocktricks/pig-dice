93724df4bc183932db15624c93a60138c225d76c
Geerocktricks <geerockface4@gmail.com>
2018-09-30 21:15:50 +0300

    This commit initiates and terminates my front end

--- js/style.js
+++ js/style.js
@@ -17,3 +17,77 @@ function getRandomNumber(lower,upper) {
   return Math.floor(Math.random()*(upper-lower + 1)) + lower;
   return dieCount;
 }
+
+// front end
+$(function() {
+  $("form#intake").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstPlayer = $("input#p1").val();
+    var inputtedSecondPlayer = $("input#p2").val();
+    newPlayer1 = new Player(inputtedFirstPlayer, 0, true);
+    newPlayer2 = new Player(inputtedSecondPlayer, 0, false);
+
+    $("#intake").hide();
+    $(".playField").show();
+    $(".playField2").show();
+
+      $("#player1name").text(newPlayer1.name);
+      $("#player2name").text(newPlayer2.name);
+      $("#player1score").text(newPlayer1.score);
+      $("#player2score").text(newPlayer2.score);
+});
+});
+
+$(function() {
+
+
+      $("button#roll1").click(function(event) {
+        event.preventDefault();
+        random = getRandomNumber(1,6);
+        if (random === 1) {
+          turnTotal1 = 0;
+          newPlayer1.turn = false;
+          newPlayer2.turn = true;
+          alert(turnTotal1);
+        } else if (random > 1) {
+          turnTotal1 += random;
+          alert(turnTotal1);
+        }
+      $("button#hold1").click(function(event) {
+        event.preventDefault();
+        newPlayer1.turn = false;
+        newPlayer2.turn = true;
+        newPlayer1.score += turnTotal1;
+        turnTotal1 = 0;
+        $("#player1score").text(newPlayer1.score);
+        $(".playerField").hide();
+      });
+    });
+  });
+
+
+  $(function() {
+
+        $("button#roll2").click(function(event) {
+          event.preventDefault();
+          random = getRandomNumber(1,6);
+          if (random === 1) {
+            turnTotal2 = 0;
+            newPlayer1.turn = true;
+            newPlayer2.turn = false;
+            alert(turnTotal2);
+          } else if (random > 1) {
+            turnTotal2 += random;
+            alert(turnTotal2);
+          }
+          $("button#hold2").click(function(event) {
+            event.preventDefault();
+            newPlayer1.turn = true;
+            newPlayer2.turn = false;
+            newPlayer2.score += turnTotal2;
+            turnTotal2 = 0;
+            $("#player2score").text(newPlayer2.score);
+      });
+    });
+});
