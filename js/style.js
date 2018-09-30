bce87ac99dcd9a880c9232f927d904a03edc76fb
Geerocktricks <geerockface4@gmail.com>
2018-09-30 20:50:56 +0300

    This commit terminates my backend (business logic)

--- js/style.js
+++ js/style.js
@@ -1,3 +1,19 @@
 // back end
 var turnTotal1 = 0;
 var turnTotal2 = 0;
+
+var random;
+var dieCount;
+var newPlayer1;
+var newPlayer2;
+
+function Player (name, score, turn) {
+  this.name = name;
+  this.score = score;
+  this.turn = turn;
+}
+
+function getRandomNumber(lower,upper) {
+  return Math.floor(Math.random()*(upper-lower + 1)) + lower;
+  return dieCount;
+}
